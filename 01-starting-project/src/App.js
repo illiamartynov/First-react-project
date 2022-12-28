54a60019dada2d56387eb209f49087fe26a8153a
a1one37git config --global user.name a1one37 <ilyamartynov989@gmail.com>
2022-12-28 01:55:46 +0100

    my first commit in my first react app

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FormResult from './FormResult';
+import UserForm from './UsertForm';
+
+
+function App() {
+  return (
+    <div>
+      <div>
+      <UserForm/>
+      <FormResult/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-starting-project/src/UserForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UserForm = (props) => {
+  return (
+    <div>
+      <form onSubmit={}>
+        <label>Enter user name</label>
+        <input type="text"></input>
+        <label>Enter user age</label>
+        <input type="text"></input>
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
